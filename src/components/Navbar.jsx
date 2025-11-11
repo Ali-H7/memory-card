dc9e0322114d833908b494e2ba273a025836bc8b
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-11 06:08:08 +0300

    Add website logo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <h1>Memory Game</h1>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
