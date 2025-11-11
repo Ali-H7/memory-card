9b33612bef5500d811f1e400a70ee523bad9e949
Ali Hassan <172230399+Ali-H7@users.noreply.github.com>
2025-11-11 09:46:25 +0300

    Lift data fetching to parent component
    This will avoid the need to fetch data from the api everytime the game
    restarts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Instructions from './components/Instructions';
 import Game from './components/Game';
-import { useState } from 'react';
+import fetchMultipleItems from './fetchData';
 
 function App() {
   const [gameKey, setGameKey] = useState(crypto.randomUUID()); // used to Restart the game
   const [bestScore, setBestScore] = useState(0);
+  const [pokemonList, setPokemonList] = useState([]);
 
   function resetGame() {
     setGameKey(crypto.randomUUID());
   }
 
+  useEffect(() => {
+    const pokemons = [
+      'bulbasaur',
+      'charmander',
+      'squirtle',
+      'pikachu',
+      'jigglypuff',
+      'eevee',
+      'snorlax',
+      'meowth',
+      'pichu',
+      'treecko',
+      'torchic',
+      'mudkip',
+    ];
+
+    async function fetchThenSetList() {
+      const list = await fetchMultipleItems(pokemons);
+      setPokemonList(list);
+    }
+    fetchThenSetList();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Instructions />
-      <Game restartGame={resetGame} key={gameKey} bestScore={bestScore} setBestScore={setBestScore} />
+      <Game
+        pokemonList={pokemonList}
+        restartGame={resetGame}
+        key={gameKey}
+        bestScore={bestScore}
+        setBestScore={setBestScore}
+      />
     </div>
   );
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,35 +1,18 @@
 import '../styles/Game.css';
-import { useState, useEffect } from 'react';
-import fetchMultipleItems from '../fetchData';
+import { useState } from 'react';
 import PokemonList from './PokemonList';
 import shuffle from 'lodash/shuffle';
 
-const pokemons = [
-  'bulbasaur',
-  'charmander',
-  'squirtle',
-  'pikachu',
-  'jigglypuff',
-  'eevee',
-  'snorlax',
-  'meowth',
-  'pichu',
-  'treecko',
-  'torchic',
-  'mudkip',
-];
-
-function Game({ restartGame, bestScore, setBestScore }) {
-  const [pokemonList, setPokemonList] = useState([]);
+function Game({ pokemonList, restartGame, bestScore, setBestScore }) {
   const [currentScore, setCurrentScore] = useState(0);
   const [playedCards, setPlayedCards] = useState([]);
 
   const shuffledList = shuffle(pokemonList);
 
   function handleOnClick(playedCard) {
-    const checkIfPlayedBefore = playedCards.includes(playedCard);
+    const hasBeenPlayed = playedCards.includes(playedCard);
 
-    if (checkIfPlayedBefore) {
+    if (hasBeenPlayed) {
       if (currentScore > bestScore) setBestScore(currentScore);
       setCurrentScore(0);
       restartGame();
@@ -38,14 +21,6 @@ function Game({ restartGame, bestScore, setBestScore }) {
       setCurrentScore(currentScore + 1);
     }
   }
-
-  useEffect(() => {
-    async function fetchThenSetList() {
-      const list = await fetchMultipleItems(pokemons);
-      setPokemonList(list);
-    }
-    fetchThenSetList();
-  }, []);
 
   return (
     <div className='game'>
